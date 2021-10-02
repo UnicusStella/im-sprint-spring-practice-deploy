5cd4ce9ee596d42ecc7772f48cb4d27170f49058
UnicusStella <unicusstella@gmail.com>
2021-10-02 15:27:43 +0900

    FIX: moidfy

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -17,7 +17,9 @@ class Main extends Component {
       <div className="main-container">
         <div className="session">토큰을 정상적으로 받았습니다</div>
         <div className="db">데이터베이스 연결 상태를 확인하세요</div>
-        <button type="submit" onClick={this.handleLogout}>로그아웃</button>
+        <button type="submit" onClick={this.handleLogout}>
+          로그아웃
+        </button>
       </div>
     );
   }
